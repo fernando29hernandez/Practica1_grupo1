9843b84c4bb7ed9fc314eaed1cb37a11bcd424c8
yoselin-19 <annelice119@gmail.com>
2018-08-22 19:35:09 -0600

    Clase Potencia_Cuadrado creada. 201403819

---
+++ src/practica1_grupo1/Potencia_Cuadrado.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_grupo1;
+
+/**
+ *
+ * @author yoselin
+ */
+public class Potencia_Cuadrado {
+    
+    public static double potencia_cuadrado (double numero) {
+	double resultado = numero * numero;
+	return resultado;
+    }
+    
+}
